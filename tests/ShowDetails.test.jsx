8bb6490bb20ed8184876b3d2d67fc71580e6eaa9
Ngaremaina <ngaremaina4@gmail.com>
2026-06-17 17:08:46 +0300

    Update test

--- tests/ShowDetails.test.jsx
+++ tests/ShowDetails.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
-import ShowDetails from "../pages/ShowDetails";
+import ShowDetails from "../src/pages/ShowDetails";
 import * as service from "../service/service";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
